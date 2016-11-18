a83e3d5487abb7cdb06fb8a07036982256614d5b
mmsamiei <mohmahsamiei@gmail.com>
2016-11-18 15:54:08 +0330

    now i can merge with fork and ashared memory :D

--- project-1/MergeFork/merge_sort.c
+++ project-1/MergeFork/merge_sort.c
@@ -5,22 +5,44 @@
 #include <sys/wait.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
-int a[10] = { 99, 14, 19, 26, 27, 31, 33, 35, 42, 44 };
-int b[10];
-
+int b[10000];
+int *shm_array;
 
 void merge_sort( int start, int end);
 void merge( int start, int mid, int end);
 
 
 int main(int argc, char *argv[]){
-
-    merge_sort( 0,9);
-    for(int i=0;i<10;i++){
-        printf("%d is %d\n",i,a[i]);
-    }
+	
+	key_t key;/* key to be passed to shmget() */ 
+	int shmflg;/* shmflg to be passed to shmget() */ 
+	int shmid;/* return value from shmget() */ 
+	int size;/* size to be passed to shmget() */ 
+	key = IPC_PRIVATE;
+	/* Create the shared memory segment. */
+	size = 10000 * sizeof(int);
+	if ((shmid = shmget(key, size, IPC_CREAT| 0660)) == -1) {
+		perror("shmget");
+		exit(1);
+	}
+	/* Attached to the shared memory segment in order to use it. */
+	if ((shm_array = shmat(shmid, NULL, 0)) == (int *) -1) {
+		perror("shmat");
+		exit(1);
+	}
+	/*
+	* Copy the data to be sorted from the local memory into the shared memory.
+	*/
+	for (int i = 0; i < 10000; i++) {
+		shm_array[i] = rand() % 10000;
+	}
+	
+	merge_sort( 0,9999);
+	for(int i=0;i<10000;i++){
+        	printf("%d is %d\n",i,shm_array[i]);
+	}
     return 0;
-}
+	}
 
 void merge_sort( int start, int end){
     if(start >= end)return;
@@ -56,18 +78,18 @@ void merge(int start, int mid, int end) {
     int l1, l2, i;
 
     for(l1 = start, l2 = mid + 1, i = start; l1 <= mid && l2 <= end; i++) {
-        if(a[l1] <= a[l2])
-            b[i] = a[l1++];
+        if(shm_array[l1] <= shm_array[l2])
+            b[i] = shm_array[l1++];
         else
-            b[i] = a[l2++];
+            b[i] = shm_array[l2++];
     }
 
     while(l1 <= mid)
-        b[i++] = a[l1++];
+        b[i++] = shm_array[l1++];
 
     while(l2 <= end)
-        b[i++] = a[l2++];
+        b[i++] = shm_array[l2++];
 
     for(i = start; i <= end; i++)
-        a[i] = b[i];
+        shm_array[i] = b[i];
 }
