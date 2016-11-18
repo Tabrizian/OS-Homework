8d1751c86742aebfa30889f943ea1f22f33f8e1c
Iman Tabrizian <Tabrizian@outlook.com>
2016-11-18 14:04:26 +0330

    Format code

--- project-1/MergeFork/Merge_sort.c
+++ project-1/MergeFork/Merge_sort.c
@@ -15,59 +15,59 @@ void merge( int start, int mid, int end);
 
 int main(int argc, char *argv[]){
 
-	merge_sort( 0,9);
-	for(int i=0;i<10;i++){
-		printf("%d is %d\n",i,a[i]);
-	}
-	return 0;
+    merge_sort( 0,9);
+    for(int i=0;i<10;i++){
+        printf("%d is %d\n",i,a[i]);
+    }
+    return 0;
 }
 
 void merge_sort( int start, int end){
-	if(start >= end)return;
-	int mid = (start+end)/2;
-	int status;
-	int lchild,rchild;
-	lchild = fork();
-	
-	if (lchild < 0) {
-		perror("fork");
-		exit(1);
-	}
-	if (lchild == 0) {
-		merge_sort( start, mid);
-		exit(0);
-	} else {
-		rchild = fork();
-		if (rchild < 0) {
-			perror("fork");
-			exit(1);
-		}
-		if (rchild == 0) {
-			merge_sort( mid+1, end);
-			exit(0);
-		}
-	}
-	waitpid(lchild, &status, 0);
-	waitpid(rchild, &status, 0);
-	merge( start, mid, end);
+    if(start >= end)return;
+    int mid = (start+end)/2;
+    int status;
+    int lchild,rchild;
+    lchild = fork();
+
+    if (lchild < 0) {
+        perror("fork");
+        exit(1);
+    }
+    if (lchild == 0) {
+        merge_sort( start, mid);
+        exit(0);
+    } else {
+        rchild = fork();
+        if (rchild < 0) {
+            perror("fork");
+            exit(1);
+        }
+        if (rchild == 0) {
+            merge_sort( mid+1, end);
+            exit(0);
+        }
+    }
+    waitpid(lchild, &status, 0);
+    waitpid(rchild, &status, 0);
+    merge( start, mid, end);
 }
 
 void merge(int start, int mid, int end) {
-   int l1, l2, i;
+    int l1, l2, i;
 
-   for(l1 = start, l2 = mid + 1, i = start; l1 <= mid && l2 <= end; i++) {
-      if(a[l1] <= a[l2])
-         b[i] = a[l1++];
-      else
-         b[i] = a[l2++];
-   }
-   
-   while(l1 <= mid)    
-      b[i++] = a[l1++];
+    for(l1 = start, l2 = mid + 1, i = start; l1 <= mid && l2 <= end; i++) {
+        if(a[l1] <= a[l2])
+            b[i] = a[l1++];
+        else
+            b[i] = a[l2++];
+    }
 
-   while(l2 <= end)   
-      b[i++] = a[l2++];
+    while(l1 <= mid)
+        b[i++] = a[l1++];
 
-   for(i = start; i <= end; i++)
-      a[i] = b[i];
+    while(l2 <= end)
+        b[i++] = a[l2++];
+
+    for(i = start; i <= end; i++)
+        a[i] = b[i];
 }
