0856715f46f6acaab50a8e2ef8f46db0973c185a
Iman Tabrizian <Tabrizian@outlook.com>
2016-11-18 11:08:27 +0330

    Path change

---
+++ project-1/Kalagh/src/child.h
@@ -0,0 +1,10 @@
+#ifndef CHILD_H
+#define CHILD_H
+
+struct child {
+    int state = 0;
+}
+
+void child_eat(struct child *current_child);
+
+#endif
---
+++ project-1/Kalagh/src/dish.h
@@ -0,0 +1,8 @@
+#ifndef DISH_H
+#define DISH_H
+
+struct dish {
+    int state;
+}
+
+#endif
