d8b9fc116eb9d6591f1a8e0f89d355f3653e9550
Iman Tabrizian <Tabrizian@outlook.com>
2017-01-13 08:45:53 +0330

    Minor improvements

--- optional-project/named-pipes/server.c
+++ optional-project/named-pipes/server.c
@@ -5,7 +5,8 @@
 #include <time.h>
 #include <string.h>
 
-int main()
+int
+main()
 {
     int fd;
     char * myfifo = "/tmp/myfifo";
@@ -16,17 +17,14 @@ int main()
     time(&rawtime);
     timeinfo = localtime (&rawtime);
 
-    /* create the FIFO (named pipe) */
     mkfifo(myfifo, 0666);
 
-    /* write "Hi" to the FIFO */
     fd = open(myfifo, O_WRONLY);
     char datetime[100];
     strcpy(datetime, asctime(timeinfo));
     write(fd, datetime, sizeof(datetime));
     close(fd);
 
-    /* remove the FIFO */
     unlink(myfifo);
 
     return 0;
